046d2113d8b66b3e9fd2e4338c733e25ceb9d556
Lukas Chiaradia <lukas.chiaradia@epitech.eu>
2024-08-28 14:14:53 +0200

    Add exercise 2 Incrament and Decrement buttons in react

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 
 class Greeting extends React.Component {
@@ -8,13 +9,27 @@ class Greeting extends React.Component {
   }
 }
 
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  function addClick() {
+    setCount(count + 1);
+  }
+  function removeClick() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       <Greeting name="John Doe" />
+      <h1>{count}</h1>
+      <button style={{ width: '200px', height: '100px' }} onClick={addClick}>+</button>
+      <button style={{ width: '200px', height: '100px' }} onClick={removeClick}>-</button>
       </header>
     </div>
+    
   );
 }
 
